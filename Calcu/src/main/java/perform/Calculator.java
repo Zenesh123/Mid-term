ae9dc5737e15b6ab15d154c305ad1397e525ccb8
Zenesh123 <100183271+Zenesh123@users.noreply.github.com>
2022-02-23 07:51:10 +0545

    new commit
    Task two added

---
+++ Calcu/src/main/java/perform/Calculator.java
@@ -0,0 +1,65 @@
+package perform;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class Calculator extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public Calculator() {
+        super();
+     
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+		throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		
+		PrintWriter out = null;
+		
+		
+		
+		try {
+			out = response.getWriter();
+			int num1 = Integer.parseInt(request.getParameter("n1"));
+			int num2 = Integer.parseInt(request.getParameter("n2"));
+			int c= 0;
+			
+			String op = request.getParameter("btn");
+			
+			if (op.equals("+")) c = num1 + num2;
+			if (op.equals("-")) c = num1 - num2;
+			if (op.equals("*")) c = num1 * num2;
+			if (op.equals("/")) c = num1 / num2;
+			
+			out.println("<center class='mt-5'><h1> Answer </h1></center>");
+			out.println("<center><h3>"+num1 + op + num2+"=" + c +"</h3></center>");
+			
+			
+			
+			
+		}catch(Exception e) {
+				out.println("Error:"+e.getMessage());
+		}
+		finally {
+				out.println("");
+		}
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
